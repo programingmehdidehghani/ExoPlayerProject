4cf5a803cb62af4183aae6434e175c6bffd53feb
mehdi <dehghani_m@rocketmail.com>
2023-04-16 11:49:52 +0430

    make contant

--- app/src/main/java/com/example/exoplayerproject/util/AudioPlayerService.kt
+++ app/src/main/java/com/example/exoplayerproject/util/AudioPlayerService.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 
 class AudioPlayerService: LifecycleService(), Player.Listener {
 
@@ -14,5 +17,16 @@ class AudioPlayerService: LifecycleService(), Player.Listener {
     private var songDescriptionLiveData = MutableLiveData<String>()
     private var songIconLiveData = MutableLiveData<Int>()
 
-    
+    private var playerNotificationPlayer: PlayerNotificationManager? = null
+   // private var binder: AudioPlayerServiceBinder()
+
+    private fun startPlayer(){
+        val context = this
+        exoPlayer = ExoPlayer.Builder(context).build()
+        val defaultDataSourceFactory = DefaultHttpDataSource.Factory()
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+
+        for (sample in C)
+    }
+
 }
