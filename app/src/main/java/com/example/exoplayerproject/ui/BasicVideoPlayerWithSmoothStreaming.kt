8be00adc01efbb2905587d9a869494de75eaff37
mehdi <dehghani_m@rocketmail.com>
2023-04-17 12:01:38 +0430

    continue make project

--- app/src/main/java/com/example/exoplayerproject/ui/BasicVideoPlayerWithSmoothStreaming.kt
+++ app/src/main/java/com/example/exoplayerproject/ui/BasicVideoPlayerWithSmoothStreaming.kt
@@ -1,14 +1,19 @@
 package com.example.exoplayerproject.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.exoplayerproject.R
 import com.example.exoplayerproject.databinding.ActivityBasicVideoPlayerWithSmoothStreamingBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.Timeline
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource
+import com.google.android.exoplayer2.source.smoothstreaming.manifest.SsManifest
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import kotlinx.android.synthetic.main.activity_basic_video_player_with_smooth_streaming.*
 
 class BasicVideoPlayerWithSmoothStreaming : AppCompatActivity() {
 
@@ -41,6 +46,28 @@ class BasicVideoPlayerWithSmoothStreaming : AppCompatActivity() {
     }
 
     private fun releasePlayer(){
-        
+        exoPlayer?.run {
+            removeListener(exoPlayerListener)
+            playbackPosition = this.currentPosition
+            currentWindow = this.currentMediaItemIndex
+            playPauseState = this.playWhenReady
+            release()
+        }
+        exoPlayer = null
+    }
+
+    inner class ExoPlayerListener: Player.Listener {
+        @SuppressLint("SetTextI18n")
+        override fun onTimelineChanged(timeline: Timeline, reason: Int) {
+            super.onTimelineChanged(timeline, reason)
+            exoPlayer?.let {
+                val manifest = it.currentManifest
+                manifest?.let {
+                    val smoothStreamManifest = it as SsManifest
+                    basic_audio_player_with_smooth_streaming_text_view.text =
+                        "durationUs = ${smoothStreamManifest.durationUs} \n dvrWindowLengthUs = ${smoothStreamManifest.dvrWindowLengthUs} \n isLive = ${smoothStreamManifest.isLive} \n lookAheadCount = ${smoothStreamManifest.lookAheadCount} \n majorVersion = ${smoothStreamManifest.majorVersion} \n minorVersion = ${smoothStreamManifest.minorVersion} \n protectionElement = ${smoothStreamManifest.protectionElement} \n streamElements = ${smoothStreamManifest.streamElements}"
+                }
+            }
+        }
     }
 }
