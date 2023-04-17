13ce0e923238d61c65be032de82a8da62592b781
mehdi <dehghani_m@rocketmail.com>
2023-04-17 10:45:21 +0430

    craeye activity media source

---
+++ app/src/main/java/com/example/exoplayerproject/ui/BasicVideoPlayerWithDashMediaSource.kt
@@ -0,0 +1,12 @@
+package com.example.exoplayerproject.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.exoplayerproject.R
+
+class BasicVideoPlayerWithDashMediaSource : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic_video_player_with_dash_media_source)
+    }
+}
